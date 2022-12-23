e89b1ff560f2c1853b96905137d9d3e5ecc6adc8
lilcryptotech <72393350+seandadonntech@users.noreply.github.com>
2022-12-23 15:24:41 -0500

    Create values.go

---
+++ values.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("123 + 123") // add operator in golang aka go
+	fmt.Println("hello", "world", "im using go!") // go does not like single quotes
+	fmt.Println(4 / 2) // make sure  to space out you signs for go to calculate correctly
+
+}
